10023f0f2c29b09e9b1c0b1178cdf69ed2ae3b80
Can <20592234+CanVo@users.noreply.github.com>
2022-07-01 15:01:57 -0500

    Added various changes.

--- src/components/pages/Credits/Credits.js
+++ src/components/pages/Credits/Credits.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import '../../../App.css';
+import './Credits.css'
 
 function Credits () {
   return (
-    <h1> Credits </h1>
+    <>
+      <div className='special-thanks'>
+        <h1> Special Thanks </h1>
+        <h2> You (for willing to read my stuff) </h2>
+      </div>
+
+      <div className='royalty-free-credit'>
+          <h1> Royalty Free Credit </h1>
+          <h2> Homepage Hero Video created by Rostislav Uzunov, taken from Pexel</h2>
+          <h2> Gem Icon used for favicon.ico by Icons8, taken from Icons8</h2>
+
+      </div>
+    </>
   );
 }
 
